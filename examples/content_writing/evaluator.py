43d74962d9f93768e8e1d128ccb7eca0065477ef
Julian <git@derjulian.net>
2025-06-13 15:06:09 +0200

    content_writing example: prompt improvement for artifact side channel feedback

--- examples/content_writing/evaluator.py
+++ examples/content_writing/evaluator.py
@@ -16,12 +16,35 @@ def evaluate_stage1(file_path):
     words_too_few_rating = 1 - max(0.0, min(1.0, (word_target - num_words) / word_target))
     words_too_many_rating = 1 - max(0.0, min(1.0, (num_words - word_target) / word_target))
 
-    return {
-        "lines_too_few": lines_too_few_rating,
-        "lines_too_many": lines_too_many_rating,
-        "words_too_few": words_too_few_rating,
-        "words_too_many": words_too_many_rating,
-    }
+    # Create combined metric
+    combined_rating = (lines_too_few_rating + lines_too_many_rating +
+                       words_too_few_rating + words_too_many_rating) / 4
+
+    # Create textual feedback
+    if num_lines > line_target*1.2:
+        length_comment = "Reduce the number of lines."
+    elif num_lines < line_target*0.8:
+        length_comment = "Increase the number of lines."
+    else:
+        length_comment = "Line number is just right, keep it that way."
+    if num_words/num_lines > word_target/line_target*1.2
+        length_comment += " Reduce the number of words per line."
+    elif num_words/num_lines < word_target/line_target*0.8:
+        length_comment += " Increase the number of words per line."
+
+    return EvaluationResult(
+        metrics={
+            "length_good": combined_rating,
+            # "lines_too_few": lines_too_few_rating,
+            # "lines_too_many": lines_too_many_rating,
+            # "words_too_few": words_too_few_rating,
+            # "words_too_many": words_too_many_rating,
+        },
+        artifacts={
+            "length_recommendation": length_comment,
+        }
+    )
+)
 
 
 def evaluate(file_path):
